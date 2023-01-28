8d4c1c56e57c69d0be4a3bcf843b695490e069d9
Zafi Shah <shah.zafira@gmail.com>
2023-01-28 00:41:33 +0000

    Update code
    No just reads from the moisture sensor

--- code.py
+++ code.py
@@ -1,44 +1,12 @@
-# import random
-# from Diamond import Diamond, Ball
-# import displayio
-# import board
-# import time
-# def collision(group,ball,diamonds):
-#     idx = -1
-#     for i, diamond in enumerate(diamonds):
-#         x1 = diamond.tl1.x
-#         y1 = diamond.tl1.y
-#         x2 = ball.posx
-#         y2 = ball. posy
-#     if d< 3:
-#         idx = i
-#     if idx >= 0:
-#         del group [idx]
-#         del diamonds[idx]
-
-# ball=Ball()
-# group=displayio.Group(max_size=10)
-# diamonds=[
-# for i in range(1):
-#     d1= Diamond(random.randint(50,230), random.randint(50,230))
-#     diamonds.append(d1)
-#     group.append(d1.getGroup())
-
-# group.append(ball.getGroup())
-# board.DISPLAY.show(group)
-# while True:
-#     ball.update()
-#     time.sleep(1/60)
-
-
 import time
 import board
-import neopixel
+from analogio import AnalogIn
 
-pixels = neopixel.NeoPixel(board.NEOPIXEL, 1)
+analog_iin = AnalogIn(board.A1)
+
+def get_voltage(pin):
+    retunr (pin.value * 3.3) / 65536
 
 while True:
-    pixels.fill((255, 0, 0))
-    time.sleep(0.5)
-    pixels.fill((0, 0, 0))
-    time.sleep(0.5)
+    print((get_voltage(analog_in),))
+    time.sleep(0.1)
