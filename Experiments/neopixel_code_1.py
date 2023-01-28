628ca40d96eb0782aa3c187127b4493341de88bb
Zafi Shah <shah.zafira@gmail.com>
2023-01-28 00:58:30 +0000

    Create neopixel
    How to control the lights for the strip LEDs

---
+++ Experiments/neopixel_code_1.py
@@ -0,0 +1,31 @@
+# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
+# SPDX-License-Identifier: MIT
+
+# This example shows how to create a single pixel with a specific color channel
+# order and blink it.
+# Most NeoPixels = neopixel.GRB or neopixel.GRBW
+# The 8mm Diffused NeoPixel (PID 1734) = neopixel.RGB
+import time
+import board
+import neopixel
+
+# Configure the setup
+PIXEL_PIN = board.D8  # pin that the NeoPixel is connected to
+ORDER = neopixel.GRB  # pixel color channel order
+COLOR = (255, 0, 0)  # color to blink
+CLEAR = (0, 0, 0)  # clear (or second color)
+DELAY = 1  # blink rate in seconds
+
+# Create the NeoPixel object
+pixel = neopixel.NeoPixel(PIXEL_PIN, 4, pixel_order=ORDER)
+
+# Loop forever and blink the color
+while True:
+    pixel[0] = COLOR
+    time.sleep(DELAY)
+    pixel[0] = CLEAR
+    time.sleep(DELAY)
+    pixel[3] = COLOR
+    time.sleep(DELAY)
+    pixel[3] = CLEAR
+    time.sleep(DELAY)
