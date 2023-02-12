3f08f9df4036f0a75dc5ae2d5723b661939ae0f6
Zafi Shah <shah.zafira@gmail.com>
2023-02-12 17:26:59 +0000

    Create Facade class with abstract methods
    The initiator requires a Creator type which has not been created yet

---
+++ Peripherals/Facade.py
@@ -0,0 +1,27 @@
+class Facade:
+    def __init__(self, Creator):
+        pass
+
+    def setColourLED(colour):
+        pass
+
+    def setIntensityLED(intensity):
+        pass
+
+    def turnOnLED():
+        pass
+
+    def turnOffLED():
+        pass
+
+    def getMoistureLevel():
+        pass
+
+    def startMotor():
+        pass
+
+    def stopMotor():
+        pass
+
+    def getTemperature():
+        pass
