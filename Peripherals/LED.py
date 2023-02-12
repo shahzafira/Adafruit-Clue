b79b4718a1ce047bfd663a6a74bafd530453f2e7
Zafi Shah <shah.zafira@gmail.com>
2023-02-12 17:29:02 +0000

    Create LED class with abstract methods
    Requires imports from neopixel

---
+++ Peripherals/LED.py
@@ -0,0 +1,15 @@
+class LED:
+    def __init__(self):
+        pass
+
+    def setColour(colour):
+        pass
+
+    def setIntensity(intensity):
+        pass
+
+    def turnOnLED():
+        pass
+
+    def turnOffLED():
+        pass
