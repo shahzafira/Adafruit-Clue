d2b9a6a435ea94a564028afeef1ee2008777f3c8
Zafi Shah <shah.zafira@gmail.com>
2023-03-14 16:12:28 +0000

    Update Motor
    Add functionality to skeleton code

--- Peripherals/Motor.py
+++ Peripherals/Motor.py
@@ -1,9 +1,19 @@
+import digitalio
+import board
 class Motor:
+    """Motor class allows the user to control the motor peripheral
+    for the greenhouse
+    """
     def __init__(self):
-        pass
+        motor_pin = digitalio.DigitalInOut(board.MISO)
+        motor_pin.direction = digitalio.Direction.OUTPUT
 
     def start():
-        pass
+        """start turns on the motor to pump the water
+        """
+        motor_pin.value = True
 
     def stop():
-        pass
+        """stop turns off the motor to stop the flow of water
+        """
+        motor_pin.value = False
